3d8068794a3eaf6d2e4989588b6a21181ced0677
eferos93 <eros.fabrici@gmail.com>
2022-03-02 19:17:43 +0100

    working on comp plot

---
+++ benchmark_comparison/benchmark_comparison.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(lubridate)
+
+
+times <- read.csv("benchmark_comparison/times.csv") %>% mutate(runtime = hms(runtime), runtime_minutes = as.numeric(hms(runtime), "minutes"))
+
+ggplot(times %>% filter(run != "snakemake"), aes(x=run, y=runtime_minutes)) +
+  geom_bar(stat = "identity") +
+  geom_hline(yintercept = times %>% filter(run == "snakemake") %>% select(runtime_minutes) %>% pull())
+
+
+
